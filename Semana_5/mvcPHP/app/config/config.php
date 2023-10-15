e52fee8d1f957583804fb4ae0e88fd1c3da27f58
itachirr <crodriguezco@est.utn.ac.cr>
2023-10-15 01:16:57 -0600

    config for house test

--- Semana_5/mvcPHP/app/config/config.php
+++ Semana_5/mvcPHP/app/config/config.php
@@ -1,15 +1,15 @@
 <?php
     //database params in constant
-    define('db_Servidor', 'localhost');
+    define('db_Servidor', '127.0.0.1');
     define('db_Basedato', 'prueba');
     define('db_Usuario', 'root');
-    define('db_Contra', 'Utn123**');
+    define('db_Contra', 'culep1711');
 
     //absolut address
     define('appRoot', dirname(dirname(__FILE__)));
 
     //relative addres
-    define('urlRoot', 'http://localhost/web2/Web_2/Semana_5/mvcPHP');
+    define('urlRoot', 'http://localhost/Web_2/Semana_5/mvcPHP');
 
     //relative address
     define('siteName', 'Ejemplo de MVC con PHP');
